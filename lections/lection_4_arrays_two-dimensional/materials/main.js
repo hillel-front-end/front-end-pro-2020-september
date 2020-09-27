d33a84b0295ed3a20c50af41d48ff819e1b0fba6
VTernavskiy <Valeriy.Ternavskiy@alfabank.kiev.ua>
2020-09-27 12:29:10 +0300

    l-4

--- lections/lection_4_arrays_two-dimensional/materials/main.js
+++ lections/lection_4_arrays_two-dimensional/materials/main.js
@@ -6,3 +6,69 @@
 
 
 
+var arr = [];
+arr.length = 5;
+
+var mass = new Array(5, 5);
+
+console.log(arr, 'arr');
+console.log(mass, 'mass');
+
+
+var twoDem = [
+  [1, 2, 3]
+];
+
+console.log(twoDem[0][1])
+
+for(var i = 0, size = 5; i < size; i++) {
+  twoDem[i] = new Array(size);
+
+  for(var j = 0; j < size; j++) {
+    twoDem[i][j] = Math.round(Math.random() * 60);
+  }
+}
+
+console.log(twoDem, 'before swap');
+var minI = 0;
+var maxI = 0;
+var sumMin;
+var sumMax;
+
+for(var i = 0, size = twoDem.length; i < size; i++) {
+  for(var j = 0, sum = 0; j < twoDem[i].length; j++) {
+    sum += twoDem[i][j];
+  }
+
+  console.log(sum, 'sum');
+
+  if (i === 0) {//Гипотиза
+    sumMin = sum;
+    sumMax = sum;
+    continue;
+  }
+
+
+  if (sum < sumMin) {
+     sumMin = sum;
+     minI = i;
+  }
+
+  if (sum > sumMax) {
+    sumMax = sum;
+    maxI = i;
+  }
+}
+
+console.log(minI, 'minI');
+console.log(sumMin, 'sumMin');
+
+console.log(maxI, 'maxI');
+console.log(sumMax, 'sumMax');
+
+
+var swap = twoDem[minI];
+twoDem[minI] = twoDem[maxI];
+twoDem[maxI] = swap;
+
+console.log(twoDem, 'after swap');
