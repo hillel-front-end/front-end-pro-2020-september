66b85b9c42402510dee99fbc1a27d8483ff9bbe0
ValeriyTernavskiy <valeraternavckiy1994@gmail.com>
2020-10-19 14:33:09 +0300

    l-8

--- lections/lection_8_LE_Scope_hoisting/materials/main.js
+++ lections/lection_8_LE_Scope_hoisting/materials/main.js
@@ -1,6 +1,6 @@
-console.log(foo, 'foo');
+// console.log(foo, 'foo');
 
-foo();
+// foo();
 
 // function toDo(a, b, callback) {
 //     console.log(callback, 'callback');
@@ -22,10 +22,10 @@ foo();
 
 // var g = 18;
 
-var foo2 = function() {
-    console.log(g, 'g --> local');
-    var g = 15;     
-};
+// var foo2 = function() {
+//     console.log(g, 'g --> local');
+//     g = 15;     
+// };
 
 // foo2();
 
@@ -33,24 +33,115 @@ var foo2 = function() {
 
 // console.log(z, 'z');
 
-if (false) {
-    var z = 12;
-}
+// if (false) {
+//     var z = 12;
+// }
 
 // console.log(z, 'z');
 
 
 
-foo();
+// foo();
 
-function foo() {
-    console.log('foo declaration');
-}
+// function foo() {
+//     console.log('foo declaration');
+// }
 
-foo();
+// foo();
 
 
 
 // --------- Lexical Environment -----------
+var c = 12;
+
+console.log(window, 'window');
 
 
+// var g = 18;
+var a = 16;
+
+// window -- global scope
+// window = {a: 16}
+
+var foo2 = function() {
+    //LE = {a: undefined, c3: function(){}}
+
+    console.log(a, 'a'); //undefined
+
+    var a = 15;     
+
+    //LE = {a: 15, c3: function(){}}
+
+    console.log(a, 'a');
+
+
+    function c3() {
+
+    }
+};
+
+// foo2();
+
+// console.log(c3, 'c3');
+// console.log(a, 'a')
+// console.log(g, 'g --> global');
+
+
+// SCOPE
+var name = 'Valera';
+
+function toDo(m) {
+    // LE-toDo = {m: undefined, f: undefined, scope: window}
+    // h = 16;
+    var f = 15;
+
+    console.log(name, 'name');
+
+    // LE-toDo = {m: undefined, f: 15, scope: window}
+
+
+    // console.log(f, 'f'); // LE-toDo.scope.f
+
+    // f = 15;
+    // console.log(f, 'f'); //LE-toDo.f
+
+    // LE = null;
+
+    // [[Scope]] -> window
+}
+
+toDo();
+
+
+// console.log(b);
+// console.log(window.h, 'window.h');
+
+
+var a = 1;
+
+// window = {a: 3}
+
+function c1() {
+    var a = 16;
+    // LE-C1 = {scope: window, c2: function(){}, a:16}
+
+    function c2() {
+        // LE-C2 = {scope: LE-C1}
+        var a = 3;
+
+        // LE-C2 = {scope: LE-C1}
+
+        console.log(a, 'a'); // LE-C2.scope.a
+
+           //[SCOPE] -> LE-C1
+    }
+
+    // LE-C1 = {scope: window, c2: function(){}, a: 16}
+    c2();
+    // LE-C1 = {scope: window, c2: function(){}, a: 3}
+
+    //[SCOPE] -> window
+ }
+
+
+ c1();
